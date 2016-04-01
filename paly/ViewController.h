2451af859c2e3d539cb5e3ea8044f12c5502d188
dc009 <dc009@hangang.lan>
2016-04-01 14:04:33 +0800

    Initial Commit

---
+++ paly/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  paly
+//
+//  Created by dc009 on 16/4/1.
+//  Copyright © 2016年 dc009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ paly/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  paly
+//
+//  Created by dc009 on 16/4/1.
+//  Copyright © 2016年 dc009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
